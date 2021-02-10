b3e79dd11ecd7aa7d97d7e6f6bf302fad2e83d54
Sebastian King <sebastian-king@users.noreply.github.com>
2021-02-10 00:59:22 -0600

    Create SMS.ino

---
+++ esp32/sms/SMS.ino
@@ -0,0 +1,75 @@
+// SIM card PIN (leave empty, if not defined)
+const char simPIN[]   = "";
+
+// Your phone number to send SMS: + (plus sign) and country code, for Portugal +351, followed by phone number
+// SMS_TARGET Example for the UK +44XXXXXXXXX
+#define SMS_TARGET  "+19401231234"
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+
+#include <TinyGsmClient.h>
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT  Serial1
+
+// Define the serial console for debug prints, if needed
+#define DUMP_AT_COMMANDS
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+void setup() {
+  // Set console baud rate
+  SerialMon.begin(115200);
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // use modem.init() if you don't need the complete restart
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
+
+  // Wait for 10 seconds while the SIM800L finishes restarting
+  delay(10000);
+  String smsMessage = String("Hello! From ESP32");
+  if (modem.sendSMS(SMS_TARGET, smsMessage)){
+    SerialMon.println("Suuccessfully sent: " + smsMessage);
+  } else {
+    SerialMon.println("SMS failed to send");
+  }
+}
+
+void loop() {
+  // do nothing
+}
