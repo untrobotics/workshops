caceea339958ba6e3e3d9b1a815a8768b3b3846c
Sebastian King <sebastian-king@users.noreply.github.com>
2021-02-14 20:53:10 -0600

    Create ESP32_with_cellular_gps.ino

---
+++ esp32/cellular+gps/modules/combined/ESP32_with_cellular_gps.ino
@@ -0,0 +1,194 @@
+// Your GPRS credentials (leave empty, if not needed)
+const char apn[]      = "h2g2"; // APN for Google Fi
+const char gprsUser[] = ""; // GPRS User
+const char gprsPass[] = ""; // GPRS Password
+
+// SIM card PIN (leave empty, if not defined)
+const char simPIN[]   = ""; 
+
+// Server details
+const char server[] = "c.massivesoft.net";
+const char resource[] = "/index.php";
+const int  port = 80;
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+#define GPSSerial Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT Serial1
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+
+#include <Wire.h>
+#include <TinyGsmClient.h>
+#include <HTTPClient.h>
+#include <Adafruit_GPS.h>
+
+// Define the serial console for debug prints, if needed
+//#define DUMP_AT_COMMANDS
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+// TinyGSM Client for Internet connection
+TinyGsmClient client(modem);
+
+Adafruit_GPS GPS(&GPSSerial);
+
+HTTPClient http;
+
+uint32_t timer = millis();
+
+void setup() {
+  // Set serial monitor debugging window baud rate to 115200
+  SerialMon.begin(115200);
+
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // use modem.init() if you don't need the complete restart
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
+  
+  delay(5000);
+
+    // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
+}
+
+void loop() {
+
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  if (millis() - timer > 10000) {
+    timer = millis(); // reset the timer
+    Serial.print("\nTime: ");
+    if (GPS.hour < 10) { Serial.print('0'); }
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    if (GPS.minute < 10) { Serial.print('0'); }
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    if (GPS.seconds < 10) { Serial.print('0'); }
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    if (GPS.milliseconds < 10) {
+      Serial.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      Serial.print("0");
+    }
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+
+      SerialMon.print("Connecting to APN: ");
+      SerialMon.print(apn);
+      if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        SerialMon.println(" FAIL");
+      } else {
+        SerialMon.println(" OK");
+        
+        SerialMon.print("Connecting to ");
+        SerialMon.println(server);
+        if (!client.connect(server, port)) {
+          SerialMon.println(" FAIL");
+        } else {
+          SerialMon.println(" OK");
+        
+          // Making an HTTP POST request
+          SerialMon.println("Performing HTTP GET request...");
+          
+          client.print(String("GET ") + resource + "?lat=" + GPS.latitude + "&lng=" + GPS.longitude + " HTTP/1.1\r\n");
+          client.print(String("Host: ") + server + "\r\n");
+          client.print("Connection: close\r\n\r\n");
+    
+          unsigned long timeout = millis();
+          while (client.connected() && millis() - timeout < 10000L) {
+            // Print available data (HTTP response from server)
+            while (client.available()) {
+              char c = client.read();
+              SerialMon.print(c);
+              timeout = millis();
+            }
+          }
+          SerialMon.println();
+        
+          // Close client and disconnect
+          client.stop();
+          SerialMon.println(F("Server disconnected"));
+          modem.gprsDisconnect();
+          SerialMon.println(F("GPRS disconnected"));
+        }
+      }
+    }
+  }
+}
